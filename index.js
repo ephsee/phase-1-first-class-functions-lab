5551b919e38c7643bfed51d6f80f85d230dd1b8c
Francois Spies <fcspies@gmail.com>
2022-01-11 12:32:12 -0500

    commit tree 27af92171a74589c78ee47e649b0c567fa9294f0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = () => drivers.slice(0, 2)
+returnFirstTwoDrivers()
+
+const returnLastTwoDrivers = () => drivers.slice(2)
+returnLastTwoDrivers()
+
+const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
+selecingDrivers
+
+createFareMultiplier()
+
+fareDoubler()
+
+fareTripler()
+
+// selectDifferentDrivers(arrayOfDrivers, function)
